8ce2546385ed4147ea1a24040f44d2cfaf7e6c3f
rishav12 <rishavqwerty7@gmail.com>
2020-07-22 21:46:07 +0530

    google auth dependencies and homepage added to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './App.css'
-
+import Homepage from './components/Homepage'
 function App() {
-  return <div>hi</div>
+  return (
+    <div>
+      <Homepage />
+    </div>
+  )
 }
 
 export default App
