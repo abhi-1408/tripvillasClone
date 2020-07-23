88f3b1e373fd8e414463a3b2d91a5cfc5ecb7d96
Abhishek Saklani <saklani1408@gmail.com>
2020-07-23 22:35:19 +0530

    added filter data and sortby

--- backend/app/main/services/filterdata.py
+++ backend/app/main/services/filterdata.py
@@ -4,27 +4,96 @@ import json
 from ..models.HotelModel import Hotel
 from ..models.RoomMetaModel import RoomMeta
 from ..models import db
+from sqlalchemy.orm import sessionmaker
+
+
+
 
 
 def filter_data(data):
-    try:
-        result = Hotel.query.join(RoomMeta,Hotel.id==RoomMeta.hotel_id)
+    # try:
+    # Session = sessionmaker()
+    # Session.configure(bind=engine)
+    # session = Session()
+    # result = db.session.query(Hotel,RoomMeta).join(RoomMeta,Hotel.id==RoomMeta.hotel_id)
+    result = db.session.query(Hotel,RoomMeta).join(RoomMeta)
+    print(result)
+    sort_by = None
+    # result =db.session.execute('''select * from hotel JOIN room_meta ON hotel.id=room_meta.hotel_id''')
+    for key in data:
+        print('key is //////\\\\\\\88888',key,data[key])
+        if key == 'hotel_id':
+            result = result.filter(RoomMeta.hotel_id == data[key])
+        elif key == 'total_units':
+            result = result.filter(RoomMeta.total_units == data[key])
+        elif key == 'max_guests':
+            result = result.filter(RoomMeta.max_guests == data[key])
+        elif key == 'price':
+            result = result.filter(RoomMeta.price == data[key])
+        elif key == 'airconditioner':
+            result = result.filter(RoomMeta.airconditioner == data[key])
+        elif key == 'cook_on_call':
+            result = result.filter(RoomMeta.cook_on_call == data[key])
+        elif key == 'parking':
+            result = result.filter(RoomMeta.parking == data[key])
+        elif key == 'spa':
+            result = result.filter(RoomMeta.spa == data[key])
+        elif key == 'number_of_bathrooms':
+            result = result.filter(RoomMeta.number_of_bathrooms == data[key])
+        elif key == 'functional_kitchen':
+            result = result.filter(RoomMeta.functional_kitchen == data[key])
+        elif key == 'internet':
+            result = result.filter(RoomMeta.internet == data[key])
+        elif key == 'pets':
+            result = result.filter(RoomMeta.pets == data[key])
+        elif key == 'dishwasher':
+            result = result.filter(RoomMeta.dishwasher == data[key])
+        elif key == 'instant_bookable':
+            result = result.filter(RoomMeta.instant_bookable == data[key])
+        elif key == 'premium':
+            result = result.filter(RoomMeta.premium == data[key])
+        elif key == 'gym':
+            result = result.filter(RoomMeta.gym == data[key])
+        elif key == 'refrigarator':
+            result = result.filter(RoomMeta.refrigarator == data[key])
+        elif key == 'television':
+            result = result.filter(RoomMeta.television == data[key])
+        elif key == 'housekeeping':
+            result = result.filter(RoomMeta.housekeeping == data[key])
+        elif key == 'swimming_pool':
+            result = result.filter(RoomMeta.swimming_pool == data[key])
+        elif key == 'washing_machine':
+            result = result.filter(RoomMeta.washing_machine == data[key])
+        elif key == 'sort_by':
+            #either asc or desc
+            sort_by = data[key]
+
+        print('temp is',result)
+    
+    if sort_by:
+        if sort_by == 'asc':
+            result = result.order_by(RoomMeta.price)
+        elif sort_by == 'desc':
+            result = result.order_by(RoomMeta.price.desc())
+        elif sort_by == 'relevence':
+            result = result.order_by(Hotel.ratings.desc())
         
-        # result =db.session.execute('''select * from hotel JOIN room_meta ON hotel.id=room_meta.hotel_id''')
-        for key in data:
-            print('key is //////\\\\\\\88888',key,data[key])
-            if key == 'internet':
-                result = result.filter(RoomMeta.internet == data[key])
-            elif key == 'gym':
-                print(type(RoomMeta.gym))
-                result = result.filter(RoomMeta.gym == data[key])
-            print('temp is',result)
 
-        res = result.all()
-        print('res *************',res)
+    res = result.all()
+    print('res *************',res)
+    temp = []
+    for a,b in res:
+        temp_dict={}
+        print(a,b)
+        temp_dict['id'] = a.id
+        temp_dict['title'] = a.title
+        temp_dict['location'] = a.location
+        temp_dict['total_units'] = a.total_units
+        temp_dict['price'] = b.price
+        temp.append(temp_dict)
 
-        return {"error": False, "message":'room created'}
-    except:
-        return {"error": True, "message":'room creation failed'}
+    return {"error": False, "message":'filtered data',"data":temp}
+    # except:
+    #     return {"error": True, "message":'filtering falied'}
 
     
