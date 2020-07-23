0029e97a1ee4543d04112ffe48adcab8e77d7b48
rishav12 <rishavqwerty7@gmail.com>
2020-07-23 23:34:18 +0530

    Added navbar &  Modified Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 import Homepage from './components/Homepage'
 function App() {
   return (
     <div>
+      <Navbar />
       <Homepage />
     </div>
   )
