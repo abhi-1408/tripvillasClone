af887c24833f82a892b5b0eed3d7fb6a955c28d8
Abhishek Saklani <saklani1408@gmail.com>
2020-07-28 13:41:39 +0530

    booking page intergrated

--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -5,16 +5,18 @@ import { FilterPage } from '../components/Filterpage'
 import { Propertypage } from '../components/Propertypage'
 import { Holidayhomes } from '../components/Holidayhomes';
 import { Map } from '../components/Map';
+import { Form } from '../components/Form';
 
 
 export const Routes = () => {
     return (
         <Switch>
-            {/* <Route path='/' exact render={(props) => <Map {...props} />} /> */}
+            {/* <Route path='/' exact render={(props) => <Form {...props} />} /> */}
             <Route path='/homepage' render={(props) => <Homepage {...props} />} />
             <Route path='/filterby' exact render={(props) => <FilterPage {...props} />} />
             <Route path='/holiday/:state' exact render={(props) => <Holidayhomes {...props} />} />
-            <Route path='/property/:id' render={(props) => <Propertypage {...props} />} />
+            <Route path='/property/:id' exact render={(props) => <Propertypage {...props} />} />
+            <Route path='/book/:id' exact render={(props) => <Form {...props} />} />
         </Switch>
     )
 }
