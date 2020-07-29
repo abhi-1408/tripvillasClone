0df023e82cf4df1b7b0c4adedd3ffbe85722ba82
rishav12 <rishavqwerty7@gmail.com>
2020-07-29 18:49:30 +0530

    commit modified Homepage,HolidayHomes page

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import styles from './Form.module.css'
 import dum3 from './imgurl/dum3.jpeg'
-<<<<<<< HEAD
-import data from './data1.json'
-export const Form = () => {
-=======
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import { Update_in_Booking } from '../Redux/common/action'
 
-
 export const Form = (props) => {
-
   let common = useSelector((state) => state.common)
   const { booking_data, booking_confirmed_details, booking_flag } = common
   let dispatch = useDispatch()
@@ -19,10 +13,9 @@ export const Form = (props) => {
   let history = useHistory()
 
   const loadRazorpay = () => {
-    return new Promise((resolve => {
-
+    return new Promise((resolve) => {
       const script = document.createElement('script')
-      script.src = "https://checkout.razorpay.com/v1/checkout.js"
+      script.src = 'https://checkout.razorpay.com/v1/checkout.js'
       script.onload = () => {
         resolve(true)
       }
@@ -30,15 +23,10 @@ export const Form = (props) => {
         resolve(false)
       }
       document.body.appendChild(script)
-    }))
-
-
+    })
   }
 
-
-
   async function displayRazorpay() {
-
     const res = await loadRazorpay()
 
     if (!res) {
@@ -47,19 +35,25 @@ export const Form = (props) => {
     }
 
     const data = await fetch('http://64651181e1b6.ngrok.io/admin/rorder', {
-      method: 'POST', body: JSON.stringify(booking_data[0])
-    }).then(t => t.json())
+      method: 'POST',
+      body: JSON.stringify(booking_data[0]),
+    }).then((t) => t.json())
     console.log('got data from razor pay as on frontend', data)
     var options = {
-      "key": "rzp_test_yGOdC4iCgylsNj", // Enter the Key ID generated from the Dashboard
-      "amount": parseInt(booking_data[0]['total_cost']['total']), // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-      "currency": 'INR',
-      "name": "Trip Villas ",
-      "description": "Property id:" + booking_data[0]['property']['id'],
-      "order_id": data['id'], //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-      "handler": function (response) {
+      key: 'rzp_test_yGOdC4iCgylsNj', // Enter the Key ID generated from the Dashboard
+      amount: parseInt(booking_data[0]['total_cost']['total']), // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency: 'INR',
+      name: 'Trip Villas ',
+      description: 'Property id:' + booking_data[0]['property']['id'],
+      order_id: data['id'], //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      handler: function (response) {
         // alert(response.razorpay_payment_id);
-        dispatch(Update_in_Booking({ ...booking_data[0], "order_id": response.razorpay_order_id }))
+        dispatch(
+          Update_in_Booking({
+            ...booking_data[0],
+            order_id: response.razorpay_order_id,
+          })
+        )
         // if (booking_flag) {
 
         //   setTimeout(() => {
@@ -71,27 +65,28 @@ export const Form = (props) => {
         // alert(response.razorpay_order_id);
         // alert(response.razorpay_signature)
       },
-      "prefill": {
-        "name": "Gaurav Kumar",
-        "email": "gaurav.kumar@example.com",
-        "contact": "9999999999"
+      prefill: {
+        name: 'Gaurav Kumar',
+        email: 'gaurav.kumar@example.com',
+        contact: '9999999999',
       },
-      "notes": {
-        "address": "Razorpay Corporate Office"
+      notes: {
+        address: 'Razorpay Corporate Office',
       },
-      "theme": {
-        "color": "#F37254"
-      }
-    };
-    var rzp1 = new window.Razorpay(options);
+      theme: {
+        color: '#F37254',
+      },
+    }
+    var rzp1 = new window.Razorpay(options)
     rzp1.open()
   }
 
   if (booking_flag) {
-    history.push('/booking-confirm/' + booking_confirmed_details['order_number'])
+    history.push(
+      '/booking-confirm/' + booking_confirmed_details['order_number']
+    )
   }
 
->>>>>>> 9f21fd769497ae0cf4f4b172d511d9d7f65584b7
   return (
     <div className='pl-5 pr-5 pb-5 pt-4 mt-3'>
       <div className='row p-2'>
@@ -123,7 +118,7 @@ export const Form = (props) => {
             <div
               className='col-6'
               style={{
-                backgroundImage: `url(${data.image_medium[0]})`,
+                backgroundImage: `url(${booking_data[0].property.image_medium[0]})`,
                 backgroundRepeat: 'no-repeat',
                 height: '160.283px',
                 width: '300px',
@@ -132,18 +127,23 @@ export const Form = (props) => {
 
             <div className='col-6'>
               <div className='m-1'>
-                <small className='text-muted'>Property Ref Id #{data.id}</small>
+                <small className='text-muted'>
+                  Property Ref Id #{booking_data[0].property.id}
+                </small>
                 <p>
-                  <b className='mt-3'>{data.title}</b>
+                  <b className='mt-3'>{booking_data[0].property.title}</b>
                   <br />
-                  <small className='text-muted'>{data.location_name}</small>
+                  <small className='text-muted'>
+                    {booking_data[0].property.location_name}
+                  </small>
                 </p>
 
                 <p>
                   <small className='text-muted'>
-                    {data.property_type} | Accommodates {data.prop_tags[3]} |{' '}
-                    {data.number_of_rooms} Bedroom(s) |{' '}
-                    {data.number_of_bathrooms} Bathroom(s)
+                    {booking_data[0].property.property_type} | Accommodates{' '}
+                    {booking_data[0].property.prop_tags[3]} |{' '}
+                    {booking_data[0].property.number_of_rooms} Bedroom(s) |{' '}
+                    {booking_data[0].property.number_of_bathrooms} Bathroom(s)
                   </small>
                 </p>
                 {/* mapping ammenties */}
@@ -156,7 +156,7 @@ export const Form = (props) => {
               <div className='text-center shadow bg-white rounded p-4 '>
                 <p className='mt-2'>
                   {' '}
-                  <b>{data.check_in}</b>
+                  <b>{booking_data[0].check_in}</b>
                 </p>
                 <p>
                   <small>Check in </small>
@@ -168,10 +168,10 @@ export const Form = (props) => {
               <div className='text-center shadow bg-white rounded p-4 '>
                 <p className='mt-2'>
                   {' '}
-                  <b>{data.check_out}</b>
+                  <b>{booking_data[0].check_out}</b>
                 </p>
                 <p>
-                  <small>Check in </small>
+                  <small>Check out</small>
                 </p>
               </div>
             </div>
@@ -180,7 +180,7 @@ export const Form = (props) => {
               <div className='text-center shadow bg-white rounded p-4 '>
                 <p className='mt-2'>
                   {' '}
-                  <b>{data.guests}</b>
+                  <b>{booking_data[0].property.max_guests}</b>
                 </p>
                 <p>
                   <small>Guests</small>
@@ -192,7 +192,7 @@ export const Form = (props) => {
               <div className='text-center shadow bg-white rounded p-4 '>
                 <p className='mt-2'>
                   {' '}
-                  <b>{data.units}</b>
+                  <b>{booking_data[0][`property`][`total_units`]}</b>
                 </p>
                 <p>
                   <small>Units </small>
@@ -204,26 +204,26 @@ export const Form = (props) => {
             <div className='mt-4'>
               Sub
               Total.....................................................................................................................
-              {data.totol_cost.sub_totol}
+              {booking_data[0].total_cost.sub_total}
             </div>
             <div className='mt-4'>
               Discount......................................................................................................................
-              {data.totol_cost.discount}
+              {booking_data[0].total_cost.discount}
             </div>
             <div className='mt-4'>
               Tax................................................................................................................................
-              {data.totol_cost.tax}
+              {booking_data[0].total_cost.tax}
             </div>
 
             <div className='mt-4'>
               Cleaning
               fee.................................................................................................................
-              {data.totol_cost.cleaning_tax}
+              {booking_data[0].total_cost.cleaning_tax}
             </div>
             <div className='mt-4'>
               <hr />
               Total..............................................................................................................................
-              {data.totol_cost.total}
+              {booking_data[0].total_cost.total}
               <hr />
             </div>
 
@@ -260,9 +260,7 @@ export const Form = (props) => {
             <div className='text-center ml-5 p-2' style={{ color: 'orange' }}>
               <b>Book fast.</b> Your dates might get booked by someone else.
             </div>
-            <div>
-              {/* BOOKING FLAG {booking_flag ? "true" : "false"} */}
-            </div>
+            <div>{/* BOOKING FLAG {booking_flag ? "true" : "false"} */}</div>
           </div>
 
           <div className=' mr-3 mb-3 ml-3 p-3 shadow bg-white rounded'>
@@ -397,9 +395,7 @@ export const Form = (props) => {
                 height: '60px',
                 background: 'rgb(30,135,240)',
                 color: 'white',
-
               }}
-
               // disabled={!message_flag}
               onClick={displayRazorpay}
             >
