983fd02c193cc099a64a2348e5ebd1f9361947d5
Abhishek Saklani <saklani1408@gmail.com>
2020-07-30 20:51:30 +0530

    added googlemaps

--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -5,6 +5,7 @@ import { FilterPage } from '../components/Filterpage'
 import { Propertypage } from '../components/Propertypage'
 import { Holidayhomes } from '../components/Holidayhomes';
 import { Map } from '../components/Map';
+import { WrappedMap } from '../components/GoogleMaps';
 import { Paymentpage } from '../components/Paymentpage';
 import { Form } from '../components/Form';
 
@@ -12,7 +13,13 @@ import { Form } from '../components/Form';
 export const Routes = () => {
     return (
         <Switch>
-            {/* <Route path='/' exact render={(props) => <Form {...props} />} /> */}
+            <Route path='/' exact render={(props) => <div style={{ width: "100vw", height: "100vh" }}>
+                <WrappedMap googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCcS0j7hDpSs-F4xDi2q6AkTD_sWqECR9M"}
+                    loadingElement={<div style={{ height: "100%" }} />}
+                    containerElement={<div style={{ height: "100%" }} />}
+                    mapElement={<div style={{ height: "100%" }} />}
+                />
+            </div>} />
             <Route path='/homepage' render={(props) => <Homepage {...props} />} />
             <Route path='/filterby' render={(props) => <FilterPage {...props} />} />
             <Route path='/holiday/:state' render={(props) => <Holidayhomes {...props} />} />
---
+++ frontend/src/components/GoogleMaps.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { Load_Filtered_Data } from '../Redux/common/action'
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from 'react-google-maps'
+
+const GoogleMaps = (props) => {
+
+    let dispatch = useDispatch()
+    let com = useSelector(state => state.common)
+    const { data } = com
+    const [selectedHotel, setSelectedHotel] = useState(null)
+
+    if (data.length > 0) {
+        // let len = Math.floor(data.length) / 2
+        return (
+
+            <GoogleMap defaultZoom={12} defaultCenter={{ "lat": parseFloat(data[0]['lat']), "lng": parseFloat(data[0]['lng']) }
+            } >
+
+                {data.map(ele => (
+                    <Marker key={ele.id} position={{ "lat": parseFloat(ele['lat']), "lng": parseFloat(ele['lng']) }}
+                        onClick={() => { setSelectedHotel(ele) }} />
+                ))}
+
+                {selectedHotel && (<InfoWindow
+                    position={{ "lat": parseFloat(selectedHotel['lat']), "lng": parseFloat(selectedHotel['lng']) }}
+                    onCloseClick={() => { setSelectedHotel(null) }}
+                >
+                    <div><h3>PropID#{selectedHotel['id']}</h3><h5>{selectedHotel['title']}</h5></div></InfoWindow>)}
+            </GoogleMap>
+
+
+        )
+    }
+    else {
+        return (
+            <div>LOADING...</div>
+        )
+    }
+}
+
+export const WrappedMap = withScriptjs(withGoogleMap(GoogleMaps));
